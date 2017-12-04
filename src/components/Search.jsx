2fe93462507e3aeaa3dc627904d84f2b2ad317dd
Tassos <lambrou@gmail.com>
2017-12-04 15:50:31 -0800

    Working search with timeout delay

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,32 +1,56 @@
 import React, {Component} from 'react'
-import TextField from 'material-ui/TextField'
+import AutoComplete from 'material-ui/AutoComplete'
 import ProgressLoader from './ProgressLoader'
-import amazon from 'amazon-product-api'
-
+import axios from 'axios'
 
 export default class Search extends Component {
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       search: "",
-      awsClient: (amazon.createClient({
-        awsId: process.env.AWSAccessKeyId,
-        awsSecret: process.env.AWSSecretKey
-      })),
       waiting: false,
       dataSource: []
     };
+    this.requestData = this.requestData.bind(this);
+  }
+
+  changeSearch(value) {
+    if (this.state.waiting == false) {
+      setTimeout(this.requestData, 2000);
+    }
+    this.setState({
+      search: value,
+      waiting: true
+    })
+  }
+
+  requestData() {
+    axios.get(`http://localhost:8000/instruments/${this.state.search}`)
+    .then((response) => {
+      console.log(response.data)
+      this.setState({ dataSource: response.data, waiting: true })
+    })
+    .catch((error) => {
+      console.log("Here is an error: ", error)
+    })
   }
 
   render() {
+
+    // Get data if there is a search term
+    if (this.state.search !== "") {
+
+    }
+
     return (
       <div className="search-component">
-        <TextField value={this.state.search}
+        {console.log("Here is the search: ", this.state.search)}
+        <AutoComplete value={this.state.search}
           hintText="Type something"
           dataSource={this.state.dataSource}
           floatingLabelText="Search"
-          onUpdateInput={this.changeSearch}
+          onUpdateInput={this.changeSearch.bind(this)}
           />
         <ProgressLoader />
         <p><span><strong>Here is your search:</strong>... {this.state.search}</span></p>
@@ -34,23 +58,7 @@ export default class Search extends Component {
     );
   }
 
-  changeSearch(value) {
-    this.setState({waiting: true})
 
-    this.state.awsClient.itemSearch({
-      keywords: value,
-      browseNodeId: '11091801',
-      responseGroup: "ItemAttributes,BrowseNodes,Images"
-    }).then(function(results){
-      console.log("We got results!: ", results[0].MediumImage[0]);
-    }).catch(function(err){
-      console.log("There was an error: ", err.Error[0].Message);
-    });
-
-    this.setState({
-      search: text
-    });
-  }
 
   getAmazonData(value) {
 
