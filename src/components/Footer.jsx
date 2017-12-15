90f86594b58c7131ba851c870e5a93f1b6704a09
Tassos <lambrou@gmail.com>
2017-12-15 01:12:24 -0600

    Minor tweaks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,74 +11,73 @@ export class Footer extends Component {
     return (
       <footer className="footer footer-black footer-big">
 
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4 col-sm-4">
-                        <div className="logo text-center">
-                            <h3>Sonic Cook</h3>
-                        </div>
-                    </div>
-                    <div className="col-md-6 offset-md-2 col-sm-8">
-                        <div className="links">
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        Home
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        Company
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        Portfolio
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                       Team
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                       Contact
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                       Join us!
-                                    </a>
-                                </li>
-                            </ul>
-                            <hr/>
-                            <div className="copyright">
-                                <div className="pull-left">
-                                    © <script>document.write(new Date().getFullYear())</script>{this.currentYear()}, made with <i className="fa fa-heart heart"></i> in San Francisco
-                                </div>
-                                <div className="pull-right">
-                                    <ul>
-                                        <li>
-                                            <a href="#">
-                                                Terms 
-                                            </a>
-                                        </li>
-                                         |
-                                        <li>
-                                            <a href="#">
-                                                 Privacy
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 col-sm-4">
+              <div className="logo text-center">
+                <h3>SonicCook.com</h3>
+              </div>
             </div>
-        </footer>
+            <div className="col-md-6 offset-md-2 col-sm-8">
+              <div className="links">
+                <ul>
+                  <li>
+                    <a href="#">
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Company
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Portfolio
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Team
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Contact
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Join us!
+                    </a>
+                  </li>
+                </ul>
+                <hr/>
+                <div className="copyright">
+                  <div className="pull-left">
+                    © {this.currentYear()}, made with <i className="fa fa-heart heart"></i> in San Francisco
+                  </div>
+                  <div className="pull-right">
+                    <ul>
+                      <li>
+                        <a href="#">
+                          Terms
+                        </a>
+                      </li>
+                      |
+                      <li>
+                        <a href="#">
+                          Privacy
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     )
   }
 }
--- src/components/SubscribeTransparent.jsx
+++ src/components/SubscribeTransparent.jsx
@@ -21,11 +21,9 @@ export class SubscribeTransparent extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col-md-9 col-sm-8">
-                        <form className="">
                             <div className="form-group">
                                 <input onChange={(e) => this.updateText(e) } value={this.state.text} type="text" className="form-control" placeholder="Enter in the name of your sound..."/>
                             </div>
-                        </form>
                     </div>
                     <div className="col-md-3 col-sm-4">
                         <button type="button" className="btn btn-primary btn-block btn-round">Get Started!</button>
