7112dc8b118abc272896172fa380f22b61e148ed
Tassos <lambrou@gmail.com>
2018-02-05 14:41:35 -0800

    Minor update

--- src/paths.js
+++ src/paths.js
@@ -1,6 +1,6 @@
 const serverPaths = {
   dev: 'http://localhost:8000',
-  prod: 'https://sonic-cook.herokuapp.com'
+  prod: 'https://missionconsultants.herokuapp.com'
 }
 
 const serverPath = (process.env.NODE_ENV === 'development') ? serverPaths.dev : serverPaths.prod
